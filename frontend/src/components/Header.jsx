439873ea5f07b7128e0b39da9a35a4e66b46b250
dev-debabrata <debabratadas711@gmail.com>
2025-07-01 23:45:13 +0530

    modified

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ import { assets } from '../assets/assets'
 const Header = () => {
     return (
         <div className=' flex items-center justify-center py-32'>
-            <div className=' flex flex-col justify-center items-center'>
+            <div className=' flex flex-col gap-10 justify-center items-center'>
                 <div >
                     <h1 className=' text-stone-800 text-6xl font-extrabold'>Hey Developer</h1>
 
