5715b979e95abac5163ec0a05844e5a24419d772
Debabrata Das <debabratadas711@gmail.com>
2026-01-09 01:05:58 +0530

    09-01-2026

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,18 @@ import cors from "cors";
 import cookieParser from 'cookie-parser';
 import path from "path";
 
-
-
 dotenv.config();
 const app = express();
 const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(cookieParser());
--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,24 +1,13 @@
 
 import auth_logo from './auth_logo.png';
-import mail_icon from './mail_icon.svg'
-import person_icon from './person_icon.svg'
 import hand_wave from './hand_wave.png'
 import header_img from './header.png'
-import arrow_icon from './arrow_icon.svg'
-import lock_icon from './lock_icon.svg'
-import auth from './auth.png';
-import auth1 from './auth1.png';
+
 
 
 export const assets = {
 
     auth_logo,
-    arrow_icon,
-    lock_icon,
-    mail_icon,
-    person_icon,
     hand_wave,
     header_img,
-    auth,
-    auth1
 }
