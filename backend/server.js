28aa54ec574f3a11c72c4fcfa63cee214430374a
Debabrata Das <debabratadas711@gmail.com>
2026-01-18 21:42:12 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ import { connectDB } from './db/connectDB.js';
 import authRoutes from "./routes/auth.route.js"
 import cors from "cors";
 import cookieParser from 'cookie-parser';
-import path from "path";
+// import path from "path";
 
 dotenv.config();
 const app = express();
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 const PORT = process.env.PORT || 5000;
 
@@ -24,13 +24,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => {
   connectDB();
